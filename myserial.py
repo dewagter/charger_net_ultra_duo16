34909edd921a25b179ad7446b290c41b2546f61c
Christophe De Wagter <dewagter@gmail.com>
2014-11-23 20:09:45 +0100

    Log as CSV

--- myserial.py
+++ myserial.py
@@ -1,9 +1,13 @@
+import time
 import serial
 import ultraduo
 import threading
 
 
 def serial_server(port):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    p = open('logfile_' + timestr + '.csv', 'w')
+    p.write(ultraduo.Charge().header()+ " " + ultraduo.Charge().header()+"\n")
     lock = threading.Lock()
     try:
         ser=serial.Serial(port, 9600, timeout=0.05)
@@ -17,11 +21,13 @@ def serial_server(port):
                 lock.acquire()
                 try:
                     ultraduo.ultraduo.parse(line)
+                    p.write(ultraduo.ultraduo.ch1.print() + " " + ultraduo.ultraduo.ch2.print() + "\n")
                 finally:
                     lock.release()
                 print(line)
                 line = ''
                 # print(ultraduo.ultraduo.ch1.print())
+        p.close()
 
     except KeyboardInterrupt:
         print ('^C received, closing serial port')
@@ -29,4 +35,4 @@ def serial_server(port):
         
 
 if __name__ == '__main__':
-    serial_server()
+    serial_server('COM11')
