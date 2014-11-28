b884fbd292892d402825576ae48b12e839687f68
Christophe De Wagter <dewagter@gmail.com>
2014-11-28 08:52:35 +0100

    Led in RUN

--- run.py
+++ run.py
@@ -9,27 +9,31 @@ import webserver
 import seriallog
 import upload
 import settings
-#import nfc
-#import led
+import nfc
+import led
 
 import ultraduo
 import imaxb6
 import charger
 
-#led.init()
-#led.blink()
+led.init()
+led.blink()
 
 
 c = ultraduo.UltraDuo()
 p = 'COM11'
 
-#try:
 #for charger in settings.chargers:
 _thread.start_new_thread( seriallog.serial_server, (p, c) )
 _thread.start_new_thread( upload.upload_server, (c, ) )
 webserver.start_webserver(c)
-#_thread.start_new_thread( nfc.nfc_server, () )
+_thread.start_new_thread( nfc.nfc_server, () )
 
 while True:
-  pass
+  if c.channel[0].newdata > 0:
+    led.toggle(led.RED)
+    c.channel[0].newdata = 0
+  if nfc.tag.new > 1:
+    led.toggle(led.ORANGE)
+    nfc.tag.new = 0
 
